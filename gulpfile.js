3b43d3188ff1ceaa29bca8a33914663950e600aa
Jeff Ye <jeffyecn@gmail.com>
2016-10-18 16:41:46 -0400

    pop script

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+var json = JSON.parse(fs.readFileSync('./package.json'));
+var version = json.version;
+
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var zip = require('gulp-zip');
 
-gulp.task('default', ['js']);
+gulp.task('default', ['js', "package"]);
 
 gulp.task('js', ['pop-under']);
 
@@ -17,5 +22,12 @@ gulp.task('pop-under', function(done) {
             .pipe(uglify({preserveComments: 'none', mangle:false}))
             .pipe(rename({extname: '.min.js'}))
             .pipe(gulp.dest('./dist'))
+            .on('end', done);
+});
+
+gulp.task('package', function(done) {
+    gulp.src('./dist/*')
+            .pipe(zip('popunder-' + version + '.zip'))
+            .pipe(gulp.dest('./docs/download'))
             .on('end', done);
 });
